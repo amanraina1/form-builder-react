1bc65e294efe5141399856b4f6748a3376edf330
amanraina1 <amanraina9086@gmail.com>
2026-05-17 12:19:43 +0530

    init

---
+++ frontend/src/router/guards.tsx
@@ -0,0 +1,28 @@
+// import { Navigate, useLocation } from "react-router-dom";
+// import type { ReactNode } from "react";
+// import { useAuthStore } from "../stores/auth";
+
+// interface Props {
+//   children: ReactNode;
+// }
+
+// export const RequireAuth = ({ children }: Props) => {
+//   const token = useAuthStore((s) => s.token);
+//   const location = useLocation();
+//   if (!token) {
+//     const redirect = location.pathname + location.search;
+//     return (
+//       <Navigate
+//         to={`/login?redirect=${encodeURIComponent(redirect)}`}
+//         replace
+//       />
+//     );
+//   }
+//   return <>{children}</>;
+// };
+
+// export const GuestOnly = ({ children }: Props) => {
+//   const token = useAuthStore((s) => s.token);
+//   if (token) return <Navigate to="/" replace />;
+//   return <>{children}</>;
+// };
